518adbd589d2fe00c4d75a109ea190450c1c149b
Pranay Bhatkar <pranaybhatkar81@gmail.com>
2025-07-26 14:37:44 +0530

    React Native Starter Template

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-red-500 flex-1 justify-center items-center border-2 m-5">
+      <Text className="text-2xl text-center border-4 border-green-800 p-2 m-3">
+        Edit src/app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
